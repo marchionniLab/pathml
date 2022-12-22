4579833c27e772a030c1d9411f0c49baa9883629
luciorq <luciorqueiroz@gmail.com>
2022-12-22 18:59:42 -0500

    feat: add callr_exec function

---
+++ R/callr_exec.R
@@ -0,0 +1,15 @@
+#' Run Function in R Session
+callr_exec <- function(func, env_name = "pathml-r") {
+  callr::r(
+    func = \(func_name, env_name) {
+      withr::local_options(list(reticulate.conda_binary = NULL))
+      withr::local_envvar(list(RETICULATE_CONDA = NULL))
+      withr::local_envvar(list(RETICULATE_MINICONDA_PATH = NULL))
+      withr::local_envvar(list(RETICULATE_PYTHON = NULL))
+      withr::local_envvar(list(RETICULATE_PYTHON = NULL))
+      rlang::exec(.fn = func_name, env_name)
+    },
+    args = list(func_name = func, env_name = env_name),
+    show = TRUE
+  )
+}
